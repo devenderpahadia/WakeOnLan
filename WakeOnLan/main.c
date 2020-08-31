fa8c108a327c05ee111c6619f141c7f47e88754f
mayankchaudhary19 <mayank1991999@gmail.com>
2020-08-31 22:26:55 +0530

    Initial Commit

---
+++ WakeOnLan/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  WakeOnLan
+//
+//  Created by Garvit Chaudhary on 31/08/20.
+//  Copyright © 2020 Garvit Chaudhary. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
